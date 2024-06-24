8ec434408fa3890b24fcbfda6db462737ced2fca
Панкин Максим <pankin475@gmail.com>
2024-06-24 13:06:04 +0300

    Initial commit

---
+++ srv/src/main/java/com/visit/card/VisitCardApplication.java
@@ -0,0 +1,11 @@
+package com.visit.card;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisitCardApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(VisitCardApplication.class, args);
+	}
+}
